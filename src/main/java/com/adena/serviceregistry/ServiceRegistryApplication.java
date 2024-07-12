2ec8c68705c1dc23cbe52d845a587809ae28ffb3
vishnu <adenavishnu1@gmail.com>
2024-07-12 22:45:57 +0530

    modified entire project

--- src/main/java/com/adena/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/adena/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.unir.eureka;
+package com.adena.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
 }
